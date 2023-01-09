7e48cf06c9bb2b9118ac9649221902d5519010bb
shabbiryk.eth <shabbirkhand.sk@gmail.com>
2023-01-09 12:25:11 +0530

    Optimized: ehr.ts

--- ehrBlockchain/contract/src/ehr.ts
+++ ehrBlockchain/contract/src/ehr.ts
@@ -10,30 +10,39 @@ import {Utils} from "./properties/Util"
 @Object()
 export class Ehr {
 
+    // The ID of this electronic health record
     @Property()
     public ehrId:string;
 
+    // The ID of the doctor who created this EHR
     @Property()
     public doctorId:string;
 
+    // The ID of the patient associated with this EHR
     @Property()
     public patientId:string;
 
+    // The symptoms the patient is experiencing
     @Property()
     public Symptoms:Symptoms;
 
+    // Any additional information about the patient's health
     @Property()
     public anyOtherProblem:string;
 
+    // The results of the patient's blood test
     @Property()
     public bloodtest:Bloodtest
 
+    // The medicines prescribed to the patient
     @Property()
     public medicines:string
 
+    // Utility functions for this EHR
     @Property()
     public util:Utils
     
+    // Feedback from the patient about their experience
     @Property()
     public patientFeedback:string;
 
