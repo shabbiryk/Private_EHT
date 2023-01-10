58c741079babac1ba4fc4871bd2745cd229148ca
shabbiryk.eth <shabbirkhand.sk@gmail.com>
2023-01-10 23:12:46 +0530

    Optimized: Patient.ts

--- ehrBlockchain/contract/src/patient.ts
+++ ehrBlockchain/contract/src/patient.ts
@@ -2,11 +2,17 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+// Import the necessary modules from the fabric-contract-api library
 import { Object, Property } from 'fabric-contract-api';
 
+// Decorate the Patient class with the @Object() decorator
+// to indicate that it will be used to define an object in a Hyperledger Fabric blockchain network
 @Object()
 export class Patient {
 
+    // Constructor for the Patient class
+    // It takes in 7 arguments: patientId,firstName,lastName,emailId,doctorId,password,ehrList
+    // And initializes the corresponding class properties with these arguments
     constructor(patientId,firstName,lastName,emailId,doctorId,password,ehrList){
         this.patientId=patientId;
         this.firstName=firstName;
@@ -17,6 +23,8 @@ export class Patient {
         this.ehrList=ehrList;
     }
 
+    // Property with decorator @Property(), which denotes that this property will be stored
+    // in the blockchain and can be accessed or modified by smart contracts that interact with the network
     @Property()
     public patientId:string;
 
@@ -37,7 +45,5 @@ export class Patient {
 
     @Property()
     public ehrList:string;
-
-    
 
 }
